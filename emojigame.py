94821a3ec01cd2e5bd2db6a0fdac1c764ae71d56
Abhinandan Dutta <144527001+Abhi69nandan@users.noreply.github.com>
2025-04-13 13:06:17 +0530

    Create emojigame.py

---
+++ emojigame.py
@@ -0,0 +1,16 @@
+# EMOJI GAME
+row1 = ["😀", "😄", "😂"]
+row2 = ["🙃", "🫠", "😉"]
+row3 = ["😇", "😍", "🥰"]
+map = [row1, row2, row3]
+
+print(f"{row1}\n{row2}\n{row3}")
+
+position = input("Where do you want to put the treasure? (e.g. 23): ")
+horizontal = int(position[0])
+vertical = int(position[1])
+
+selected_row = map[vertical - 1]
+selected_row[horizontal - 1] = "X"
+
+print(f"{row1}\n{row2}\n{row3}")
